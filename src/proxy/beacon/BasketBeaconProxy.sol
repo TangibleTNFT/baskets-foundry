0ce99dbafaeda8505ae01bf5181cbbe4698d9572
Chase Brown <Chasebrown7@icloud.com>
2023-10-31 13:42:36 -0700

    new beacon inheritance design

--- src/proxy/beacon/BasketBeaconProxy.sol
+++ src/proxy/beacon/BasketBeaconProxy.sol
@@ -1,28 +1,14 @@
-// SPDX-License-Identifier: MIT
-// OpenZeppelin Contracts (last updated v5.0.0) (proxy/beacon/BeaconProxy.sol)
-
+// SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.20;
 
+import { BeaconProxy } from "@openzeppelin/contracts/proxy/beacon/BeaconProxy.sol";
 import { IBeacon } from "@openzeppelin/contracts/proxy/beacon/IBeacon.sol";
-import { Proxy } from "@openzeppelin/contracts/proxy/Proxy.sol";
-import { ERC1967Utils } from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Utils.sol";
 
 /**
- * @dev This contract implements a proxy that gets the implementation address for each call from an {UpgradeableBeacon}.
- *
- * The beacon address can only be set once during construction, and cannot be changed afterwards. It is stored in an
- * immutable variable to avoid unnecessary storage reads, and also in the beacon storage slot specified by
- * https://eips.ethereum.org/EIPS/eip-1967[EIP1967] so that it can be accessed externally.
- *
- * CAUTION: Since the beacon address can never be changed, you must ensure that you either control the beacon, or trust
- * the beacon to not upgrade the implementation maliciously.
- *
- * IMPORTANT: Do not use the implementation logic to modify the beacon storage slot. Doing so would leave the proxy in
- * an inconsistent state where the beacon storage slot does not match the beacon address.
+ * @dev This is a custom contract. It adds 2 helper methods to the BeaconProxy pattern.
+ * This makes testing state easier when we're able to pull beacon data from the proxy.
  */
-contract BasketBeaconProxy is Proxy {
-    // An immutable address for the beacon to avoid unnecessary SLOADs before each delegate call.
-    address private immutable _beacon;
+contract BasketBeaconProxy is BeaconProxy {
 
     /**
      * @dev Initializes the proxy with `beacon`.
@@ -36,24 +22,7 @@ contract BasketBeaconProxy is Proxy {
      * - `beacon` must be a contract with the interface {IBeacon}.
      * - If `data` is empty, `msg.value` must be zero.
      */
-    constructor(address beacon, bytes memory data) payable {
-        ERC1967Utils.upgradeBeaconToAndCall(beacon, data);
-        _beacon = beacon;
-    }
-
-    /**
-     * @dev Returns the current implementation address of the associated beacon.
-     */
-    function _implementation() internal view virtual override returns (address) {
-        return IBeacon(_getBeacon()).implementation();
-    }
-
-    /**
-     * @dev Returns the beacon.
-     */
-    function _getBeacon() internal view virtual returns (address) {
-        return _beacon;
-    }
+    constructor(address beacon, bytes memory data) BeaconProxy(beacon, data) payable {}
 
     /**
      * @dev This method is mainly for testing purposes. Allows us to fetch externally the implementation address from beacon.
